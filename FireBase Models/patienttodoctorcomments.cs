62cd0ab5fd86cdfe74d760acdeaeb7e550af0e06
Joshua Weg <joshua.weg.95@gmail.com>
2022-11-18 19:39:44 -0800

    added patienttodoctorcomments class

---
+++ FireBase Models/patienttodoctorcomments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebaseConnector.Models
+{
+    internal class patienttodoctorcomments
+    {
+        public int ID { get; set; }
+        public string doctor { get; set; }
+        public string patient { get; set; }
+        public string message { get; set; }
+    }
+}
